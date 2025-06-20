15679e9565b5853c3e449a9767332021c77d5a32
Mirror83 <glenochieng045@gmail.com>
2025-06-20 14:07:43 +0300

    feat: implement the parse_t function

--- src/parser.rs
+++ src/parser.rs
@@ -18,6 +18,7 @@ impl Error for ParseError {}
 
 pub struct Parser {
     tokens: Vec<Token>,
+    current_token_index: usize,
 }
 
 impl Parser {
@@ -26,16 +27,62 @@ impl Parser {
         if tokens.len() == 0 {
             Err(ParseError::NoInput)
         } else {
-            Ok(Parser { tokens })
+            Ok(Parser {
+                tokens,
+                current_token_index: 0,
+            })
         }
     }
 
     /// Parses the arithmetic expression represented by the token list
     /// and returns the result of that expression, or an error
-    /// if the original expression was malformed
-    pub fn parse(&self) -> Result<i32, ParseError> {
+    /// if the original expression was malformed.
+    /// Corresponds to the `P -> E` production in the grammar.
+    pub fn parse(&self) -> Result<u32, ParseError> {
         Ok(1)
     }
+
+    /// Corresponds to the `E -> TE'` production in the grammar.
+    fn parse_e(&self) -> Result<u32, ParseError> {
+        Ok(1)
+    }
+
+    /// Corresponds to the `E' -> +TE | ϵ` production in the grammar.
+    fn parse_e_prime(&self) -> Result<u32, ParseError> {
+        Ok(1)
+    }
+
+    /// Corresponds to the `T -> (E) | num` production in the grammar.
+    fn parse_t(&self) -> Result<u32, ParseError> {
+        let token = self.next_token();
+        match token {
+            Token::LBracket => {
+                let inner_expression_result = self.parse_e()?;
+                if !self.expect_token(Token::RBracket) {
+                    Err(ParseError::GenericError)
+                } else {
+                    Ok(inner_expression_result)
+                }
+            }
+            Token::Num(val) => Ok(val),
+            _ => Err(ParseError::GenericError),
+        }
+    }
+
+    /// Returns the next token to be considered
+    fn next_token(&self) -> Token {
+        Token::Num(1)
+    }
+
+    /// Returns `true` if [t] is the same as the next token
+    /// and false otherwise
+    fn expect_token(&self, t: Token) -> bool {
+        return false;
+    }
+
+    /// Puts an unexpected token [t] back for consideration. That token
+    /// will be returned again by the next call to `next_token`
+    fn putback_token(&self, t: Token) {}
 }
 
 #[cfg(test)]
