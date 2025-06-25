2c86e04cc702c269095a14b87c91d5fdf657d9b1
Mirror83 <glenochieng045@gmail.com>
2025-06-25 13:36:30 +0300

    refactor: specify parent for the run function
    it is idiomatic to specify parents when calling functions according to the Rust book

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use recursive_descent_calculator::run;
-
 fn main() {
     println!("Recursive descent calculator.");
     println!("Press CTRL+C to quit");
-    run();
+    recursive_descent_calculator::run();
 }
